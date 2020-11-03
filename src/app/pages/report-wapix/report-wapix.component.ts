4f3667a825e6c5df63242ba3c5f6f35a148fabb5
Aris Cruz <ariscruz0117@gmail.com>
2020-11-03 17:44:36 -0600

    Report service added, report view working with back-end

--- src/app/pages/report-wapix/report-wapix.component.ts
+++ src/app/pages/report-wapix/report-wapix.component.ts
@@ -1,7 +1,11 @@
 /* Font-awesome Icons */
-import { faSearchPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearchPlus, faSearch, faSurprise } from '@fortawesome/free-solid-svg-icons';
 
+/* imports */
 import { Component, OnInit } from '@angular/core';
+import { WapixService } from 'src/app/globals/services/wapix.service';
+import { environment } from '../../../environments/environment';
+import { ReportService } from 'src/app/globals/services/report.service';
 
 
 @Component({
@@ -13,11 +17,44 @@ export class ReportWapixComponent implements OnInit {
 
   faSearchPlus =faSearchPlus;
   faSearch =faSearch;
+  faSurprise = faSurprise;
+
+  wapixes:Array<any> = [];
+  report:Array<any> = [];
+  token:string = environment.token;
 
 
-  constructor() { }
+  constructor(private wapixService:WapixService, private reportService:ReportService) { }
 
   ngOnInit(): void {
+    /*
+      Obtain the token and the email from the session,
+      for now, it is hardcoded.
+    */
+    let email:string = 'gare_98@hotmail.com';
+    let token:string = environment.token;
+
+    this.wapixService.getWapixFromUser(email, token)
+      .then( data => {
+        this.wapixes = data.wapix;
+      })
+      .catch( err => {
+        console.error(err);
+      });
+
+  }
+
+  getResultByWapixId(){
+
+    let wapix:any = document.getElementById('selectWapix');
+    let id = wapix.options[wapix.selectedIndex].value;
+    
+    this.reportService.getResultByWapixId(id, this.token)
+      .then( data => {
+        this.report = data.result;
+        console.log('Results obtained from wapix', id);
+        console.log('result: ' ,this.report);
+    });
   }
 
 }
