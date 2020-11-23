a7eaa1cf14b7532c7521939d9db5b182553caddc
GustavoRuedaEnriquez <gusgus2208@gmail.com>
2020-11-22 18:39:52 -0600

    Add activate/deactivate functions.

--- src/app/globals/services/wapix.service.ts
+++ src/app/globals/services/wapix.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../environments/environment';
+import { ThrowStmt } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -48,6 +49,22 @@ export class WapixService {
       'Authorization' : `${token}`
     }
     return this.httpClient.patch(url,wapix,{ headers : requestHeaders }).toPromise();
+  }
+
+  activateWapix(wapixId:string, token:string):Promise<any> {
+    let url:string = environment.apiUrl + `wapix-activate/${wapixId}`;
+    let requestHeaders = {
+      'Authorization' : `${token}`
+    }
+    return this.httpClient.patch(url,{},{ headers : requestHeaders }).toPromise();
+  }
+
+  deactivateWapix(wapixId:string, token:string):Promise<any> {
+    let url:string = environment.apiUrl + `wapix-deactivate/${wapixId}`;
+    let requestHeaders = {
+      'Authorization' : `${token}`
+    }
+    return this.httpClient.patch(url,{},{ headers : requestHeaders }).toPromise();
   }
 
   enterWapixCode(code:string):Promise<any> {
