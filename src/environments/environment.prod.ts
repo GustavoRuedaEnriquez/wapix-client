e952b4b9b45f85417f8420af7d58ed27f4c85cfd
ArisCruz0117 <is708067@iteso.mx>
2020-12-01 11:30:56 -0600

    clientId updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://wapix-server.herokuapp.com/api/',
   socketUrl: 'https://wapix-server.herokuapp.com/',
-  clientId: '763702996895-d1j6oq8bqf4vrpjn4kpmvv18hl8g6tu6.apps.googleusercontent.com'
+  clientId: '763702996895-6mopudgr7p9traenj4m0sggo5859jhud.apps.googleusercontent.com'
 };
